2148edea9305b6579dff6c5c3e5b20449a27d110
EC2 Default User <163057625+tomotomo11@users.noreply.github.com>
2024-04-13 08:40:24 +0000

    [Rest4]3-16

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       render :new
@@ -14,6 +15,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -27,14 +29,26 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "Book was successfully created."
     redirect_to book_path(book.id)
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully created."
     redirect_to '/books'
   end
+
+#  def destroy
+#    @book = Book.find(params[:id])
+#    if @book.destroy
+#      flash[:success] = "Book was successfully created."
+#    else
+#      flash[:error] = "error"
+#    end
+#      redirect_to books_path
+#  end
 
   private
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
-  get '/top' => 'homes#top'
+  root 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
